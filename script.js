9ad6359aa830802278006923a3c8c33cb552ffe2
Imam Hasan <mdimamhasanihr@gmail.com>
2024-11-02 07:45:27 +0600

    removed previous to solve conflict

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional JavaScript for smooth scrolling between sections
+document.querySelectorAll('.nav-link').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
