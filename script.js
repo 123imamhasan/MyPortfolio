bf99854c3bf5f358b663192248597e62a727ac0e
Imam Hasan <mdimamhasanihr@gmail.com>
2024-11-02 13:46:24 +0600

    rm-dark,update about section by hover effet
    solve bug home-scrool

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Dark Mode Toggle
-const toggleButton = document.getElementById('dark-mode-toggle');
-
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
 
 /* Smooth Scrolling */
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
